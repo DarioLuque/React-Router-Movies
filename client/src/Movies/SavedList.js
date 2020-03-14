dbbd7afc530a819f3ca11e086d8b7351ef094e2a
Dario Luque <dario.lu92@gmail.com>
2020-03-13 23:30:24 -0700

    Stretch -Make saved into link- complete

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import MovieList from './MovieList';
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+      <Link to={`/movies/${movie.id}`} className="saved-movie">{movie.title}
+
+      </Link>
     ))}
 
     <NavLink to="/" className="home-button">Home</NavLink>
